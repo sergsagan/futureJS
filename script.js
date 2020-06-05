3f8f18bdf818501fe5bba8eba8db0a8e3f88b8d5
sagan <sagan@syneforge.com>
2020-06-05 16:49:28 +0300

    commit v2

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 50000;
+let income = 'фриланс';
+let addExpenses = 'Интернет, Такси, Коммуналка';
+let deposit = true;
+let mission = 1000000;
+let period = 12;
+let budgetDay = money/30;
 
-alert('Javascript');
-console.log('test');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цуль заработать ' + mission + ' долларов');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+console.log(budgetDay);
